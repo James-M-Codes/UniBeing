733b01f86cbd5203f499eea1f6a8893698a92adb
OnyxCrow22 <l.hirst3@uni.brighton.ac.uk>
2026-03-11 14:53:20 +0000

    Fixed Text-to-Speech bug
    Fixed the text to speech bug by changing the code from document.querySelector('.bot-message:last-child') to sEvent.currentTarget.closest('.bot-message').innerText!

--- TextToSpeech.js
+++ TextToSpeech.js
@@ -1,11 +1,12 @@
 window.addEventListener("load", function()
 {
   console.log("Button found! And I can click it!")
+  console.log("Reading!");
 });
 
-function speakText() {
+function speakText(sEvent) {
   // Create a SpeechSynthesisUtterance
-  let speechText = document.querySelector('.bot-message:last-child');
+  let speechText = sEvent.currentTarget.closest('.bot-message').innerText;
   const utterance = new SpeechSynthesisUtterance(speechText);
 
   // Speak the text
