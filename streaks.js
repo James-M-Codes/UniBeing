6efe5eafb1658281bed95fb4df194af695ba3e85
Sam160412 <samsodki04@icloud.com>
2026-04-28 23:51:07 +0100

    Gamification Js now uses safelocalstorageget from security js to allow for more security

--- streaks.js
+++ streaks.js
@@ -6,7 +6,7 @@ const DEFAULT_USER_DATA = {
     level: 1,
 };
 function getUserData() {
-    const rawData = localStorage.getItem('uniBeingData');
+    const rawData = safeLocalStorageGet('uniBeingData');
     if (!rawData) {
         localStorage.setItem('uniBeingData', JSON.stringify(DEFAULT_USER_DATA));
         return { ...DEFAULT_USER_DATA };
